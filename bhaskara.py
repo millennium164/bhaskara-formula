c559de86183ee1b579b2ec0baf9251d5ec674813
millennium_yr <milleneyoshinorusso@outlook.com>
2024-08-28 19:27:31 -0300

    translated to english

--- bhaskara.py
+++ bhaskara.py
@@ -6,7 +6,7 @@ def main():
     if delta == 0:
         x = -b/(2*a)
         print("x = ", round(x, 4))
-        print("uma raiz real")
+        print("one real solution")
     else:
         # Delta > 0
         if delta > 0:
@@ -14,22 +14,22 @@ def main():
             x2 = (-b - delta**(1/2))/(2*a)
             print("x1 = ", round(x1, 4))
             print("x2 = ", round(x2, 4))
-            print("duas raizes reais")
+            print("two real solutions")
         # Delta < 0
         else:
             xr = -b/(2*a)
             xim = (-delta)**(1/2)/(2*a)
             print("x1 = ", round(xr, 4), " + ",round(xim, 4), "i")
             print("x2 = ", round(xr, 4), " - ",round(xim, 4), "i")
-            print("duas raizes complexas")
+            print("two complex solutions")
 
 # Prompt for coeficients of quadratic function
 def get_coeficients():
   while True:
     try:
-        a = float(input("coeficiente a: "))
-        b = float(input("coeficiente b: "))
-        c = float(input("coeficiente c: "))
+        a = float(input("coeficient a: "))
+        b = float(input("coeficient b: "))
+        c = float(input("coeficient c: "))
         return a, b, c
     except ValueError:
         print("Not a float")
